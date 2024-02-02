2a6e928b51c47f28e8c1b7e35d0c8cbb8f67c7c1
Tatiana Lazarenko <tan.strebkova@gmail.com>
2024-02-02 17:11:40 +0300

    RGBSlider's logic was made

--- RGBSlider/ViewController.swift
+++ RGBSlider/ViewController.swift
@@ -7,13 +7,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var coloredView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupColorLabels()
+        coloredView.layer.cornerRadius = 20
     }
 
-
+    @IBAction func redSliderAction() {
+        redLabel.text = getRoundedValue(redSlider.value)
+        createCurrentColor()
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenLabel.text = getRoundedValue(greenSlider.value)
+        createCurrentColor()
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueLabel.text = getRoundedValue(blueSlider.value)
+        createCurrentColor()
+    }
+    
+    private func setupColorLabels() {
+        redLabel.text = redSlider.value.formatted()
+        greenLabel.text = greenSlider.value.formatted()
+        blueLabel.text = blueSlider.value.formatted()
+    }
+    
+    private func getRoundedValue(_ value: Float) -> String? {
+        let roundedValue = round(value * 100) / 100.0
+        return roundedValue.formatted()
+    }
+    
+    private func createCurrentColor() {
+        let currentColor = CGColor(
+            red: CGFloat(
+                redSlider.value
+            ),
+            green: CGFloat(
+                greenSlider.value
+            ),
+            blue: CGFloat(
+                blueSlider.value
+            ),
+            alpha: 1
+        )
+        
+        coloredView.backgroundColor = UIColor(cgColor: currentColor)
+    }
 }
 
